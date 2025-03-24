61e1f2efe30628cee3e5542c2bb6b468c9149674
MATMaucio <marellanotsimogianis@gmail.com>
2025-03-24 15:45:54 -0600

    Correciones a fantasmas

--- Assets/ScriptsMau/Scripts Type Ghosts/SthealGhost.cs
+++ Assets/ScriptsMau/Scripts Type Ghosts/SthealGhost.cs
@@ -5,13 +5,21 @@ public class StealthGhost : EnemyBase
     public float teleportDistance = 5f; // Distancia mínima para teletransportarse cerca del jugador
 
     protected override void ShootAtPlayer(GameObject player)
-    {
-        Vector3 teleportPosition = player.transform.position + Random.onUnitSphere * teleportDistance;
-        teleportPosition.y = firePoint.position.y; // Mantén la altura del fantasma
-        transform.position = teleportPosition;
+{
+    // Calcula la posición base enfrente del jugador
+    Vector3 teleportPosition = player.transform.position + player.transform.forward * teleportDistance;
 
-        base.ShootAtPlayer(player);
+    // Agrega un pequeño desplazamiento aleatorio en los ejes x y z
+    teleportPosition += new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
 
-        Debug.Log("Fantasma sigiloso se teletransportó y disparó");
-    }
+    // Eleva ligeramente al fantasma para que no aparezca en el piso
+    teleportPosition.y = player.transform.position.y + 1f;
+
+    // Teletransporta al fantasma
+    transform.position = teleportPosition;
+
+    base.ShootAtPlayer(player);
+
+    Debug.Log("Fantasma sigiloso se teletransportó enfrente del jugador con un ligero desplazamiento y disparó");
+}
 }
