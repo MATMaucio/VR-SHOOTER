f7bee39e8a056fb0d05df60b46c6058c159969c2
Antonio1886 <107732360+Antonio1886@users.noreply.github.com>
2025-03-25 22:16:18 -0600

    pool objetos

--- Assets/ScriptsDiego/VRShoot.cs
+++ Assets/ScriptsDiego/VRShoot.cs
@@ -1,23 +1,63 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System.Collections;
-using UnityEngine.Rendering;
+using System.Collections; 
 public class VRShoot : MonoBehaviour
 {
     [SerializeField] private AudioClip revolverSoundClip;
-    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private int poolSize = 10;
 
-    public void fireBullet(){
-    AudioShoot();
-    GameObject spawBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
-     spawBullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * bulletSpeed;
-    Destroy (spawBullet, 3f);
+    private Queue<GameObject> bulletPool = new Queue<GameObject>();
 
+    private void Start()
+    {
+        InitializePool();
     }
 
-    public void AudioShoot(){
+    private void InitializePool()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.SetActive(false);
+            bulletPool.Enqueue(bullet);
+        }
+    }
+
+    public void FireBullet()
+    {
+        AudioShoot();
+        GameObject bullet = GetBulletFromPool();
+        if (bullet != null)
+        {
+            bullet.transform.position = spawnPoint.position;
+            bullet.transform.rotation = spawnPoint.rotation;
+            bullet.SetActive(true);
+            bullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * bulletSpeed;
+            StartCoroutine(DeactivateBulletAfterTime(bullet, 3f));
+        }
+    }
+
+    private GameObject GetBulletFromPool()
+    {
+        if (bulletPool.Count > 0)
+        {
+            return bulletPool.Dequeue();
+        }
+        return null;
+    }
+
+    private IEnumerator DeactivateBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        bullet.SetActive(false);
+        bulletPool.Enqueue(bullet);
+    }
+
+    public void AudioShoot()
+    {
         AudioManager.instance.PlaySound(revolverSoundClip);
     }
 }
---
+++ Assets/Tona/TonaScripts/ObjectMove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ObjectMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private PoolObject cactusPool;
+
+    void Awake()
+    {
+        cactusPool = Object.FindAnyObjectByType<PoolObject>();
+    }
+
+    void Update()
+    {
+        if (gameObject.activeInHierarchy)  // Solo mueve el cactus si está activo
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Respawn"))
+        {
+            Debug.Log("Cactus colisionó con Respawn");
+            cactusPool.ReturnCactus(gameObject);
+        }
+    }
+}
