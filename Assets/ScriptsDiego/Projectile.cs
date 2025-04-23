4960355f40e8ebaa987dc25db65e06615cc028ee
MATMaucio <marellanotsimogianis@gmail.com>
2025-04-23 15:53:56 -0600

    Efectos para el player

--- Assets/ScriptsDiego/Projectile.cs
+++ Assets/ScriptsDiego/Projectile.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
 
+public enum ProjectileType
+{
+    Normal,
+    Fire,
+    Ice,
+    Poison
+}
+
 public class Projectile : MonoBehaviour
 {
     public float damage = 15f;
     public float lifetime = 5f;
-    
+    public ProjectileType type = ProjectileType.Normal;
+
     void Start()
     {
         Destroy(gameObject, lifetime);
     }
-    
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            // El jugador manejará el daño a través de su PlayerHealth
+            // 🔥 Si es de tipo fuego, activa el efecto de quemadura
+            if (type == ProjectileType.Fire)
+            {
+                FireDamageEffect fireEffect = collision.gameObject.GetComponent<FireDamageEffect>();
+                if (fireEffect != null)
+                {
+                    fireEffect.TriggerBurn();
+                }
+            }
+
+            // El daño se aplica en PlayerHealth, así que este script solo destruye
             Destroy(gameObject);
         }
         else if (!collision.gameObject.CompareTag("Enemy"))
